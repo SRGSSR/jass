aa7401ec50fe6ccb477b2b91a45ad0ade74af9a1
Cédric Foellmi <cedric@onekilopars.ec>
2015-09-15 17:07:59 +0200

    Much better collapsing panels for requests

--- project/static/javascripts/inputrequests/directives/inputrequest.directive.js
+++ project/static/javascripts/inputrequests/directives/inputrequest.directive.js
@@ -23,7 +23,15 @@
             scope: {
                 inputrequest: '='
             },
-            templateUrl: '/static/templates/inputrequests/inputrequest.html'
+            templateUrl: '/static/templates/inputrequests/inputrequest.html',
+            link: function(scope, element, attrs) {
+                angular.element('.btn-details').on('click', function(e) {
+                    e.preventDefault();
+                    var $this = $(this);
+                    var $collapse = $this.closest('.collapse-group').find('.collapse');
+                    $collapse.collapse('toggle');
+                })
+            }
         };
 
         return directive;
