460d90683b106ae34f26006d4f7f9a3b7c1648f9
Cédric Foellmi <cedric@onekilopars.ec>
2015-09-15 15:02:59 +0200

    Removed unnecessary check

--- project/static/javascripts/layout/controllers/index.controller.js
+++ project/static/javascripts/layout/controllers/index.controller.js
@@ -48,21 +48,19 @@
 
                 for (var i = 0; i < vm.inputrequests.length; i++) {
                     var inputrequest = vm.inputrequests[i];
-                    if (inputrequest.url.indexOf('b.scorecardresearch.com') > -1) {
-                        (function(req) {
-                            var parser = document.createElement('a');
-                            parser.href = req.url;
-                            var tmp_arguments = parser.search.split('&');
-                            req.request_arguments = [];
-                            for (var j = 0; j < tmp_arguments.length; j++) {
-                                var arg = tmp_arguments[j].split('=');
-                                if (arg[0][0] === "?") {
-                                    arg[0] = arg[0].substring(1);
-                                }
-                                req.request_arguments.push({'key': arg[0], 'value': arg[1]});
+                    (function(req) {
+                        var parser = document.createElement('a');
+                        parser.href = req.url;
+                        var tmp_arguments = parser.search.split('&');
+                        req.request_arguments = [];
+                        for (var j = 0; j < tmp_arguments.length; j++) {
+                            var arg = tmp_arguments[j].split('=');
+                            if (arg[0][0] === "?") {
+                                arg[0] = arg[0].substring(1);
                             }
-                        })(inputrequest);
-                    }
+                            req.request_arguments.push({'key': arg[0], 'value': arg[1]});
+                        }
+                    })(inputrequest);
                 }
             }
 
