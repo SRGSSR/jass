fe1473b96bab755f039502ca157af40525b54b1c
Cédric Foellmi <cedric@onekilopars.ec>
2015-09-24 15:20:51 +0200

    Inverse urls for accessing admin/

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('project.jass.urls', namespace="jass")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('project.jass.urls', namespace="jass")),
 ]
