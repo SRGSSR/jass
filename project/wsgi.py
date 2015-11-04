5d32ddcb187172aa4abc08229f7fce0b609c1fd5
Cédric Foellmi <cedric@onekilopars.ec>
2015-11-04 14:30:51 +0100

    Preparing for prod.

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,12 +8,22 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jass.settings")
 
-from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 
-application = Cling(get_wsgi_application())
+if settings.DEBUG:
+    application = Cling(get_wsgi_application())
+else:
+    _django_app = Cling(get_wsgi_application())
+    _websocket_app = uWSGIWebsocketServer()
+
+    def application(environ, start_response):
+        if environ.get('PATH_INFO').startswith(settings.WEBSOCKET_URL):
+            return _websocket_app(environ, start_response)
+        return _django_app(environ, start_response)
+
+
