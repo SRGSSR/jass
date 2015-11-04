a2ac7226567aa222336c425477b603679ea7e073
Cédric Foellmi <cedric@onekilopars.ec>
2015-11-04 12:03:06 +0100

    Removed comments

--- project/static/javascripts/jass.config.js
+++ project/static/javascripts/jass.config.js
@@ -7,11 +7,6 @@
 
     config.$inject = ['$locationProvider'];
 
-    /**
-     * @name config
-     * @desc Enable HTML5 routing
-     * See https://thinkster.io/django-angularjs-tutorial/
-     */
     function config($locationProvider) {
         $locationProvider.html5Mode(true);
         $locationProvider.hashPrefix('!');
