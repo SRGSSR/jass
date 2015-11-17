6dbd5520b47f1ec5eb38c2c249e160b72bedf11d
Cédric Foellmi <cedric@onekilopars.ec>
2015-11-17 17:21:59 +0100

    Update of start script

--- tools/proxy/start.sh
+++ tools/proxy/start.sh
@@ -1,7 +1,7 @@
-#! /bin/sh
+#! /bin/bash
 PORT=8080
-JASS_SERVER="http://jass-prod.herokuapp.com"
-JASS_API="$JASS_SERVER/api/inputrequests/"
+JASS_PROD_SERVER="http://jass-prod.herokuapp.com"
+JASS_STAGING_SERVER="http://jass-staging.herokuapp.com"
 
 if ! which mitmdump >/dev/null ; then
   cat <<EOF
@@ -17,10 +17,16 @@ if ! python test_imports.py ; then
   exit
 fi
 
+JASS_SERVER=$JASS_PROD_SERVER
+if [[ "$1" == "staging" || "$1" == "2" ]] ; then
+  JASS_SERVER=$JASS_STAGING_SERVER
+fi
+
 echo Proxy started. The IP address to use as Manual Proxy in test devices is
 for i in `ifconfig |grep inet |grep -v inet6 |cut -d ' ' -f 2` ; do
   echo $i:8080
 done
 echo You can see the results at: $JASS_SERVER
 
+JASS_API="$JASS_SERVER/api/inputrequests/"
 mitmdump -p $PORT -s "sendToServer.py $JASS_API"
