c6c209a1e646dcb6110c1d9c8a434f1aec3a6f20
Cédric Foellmi <cedric@onekilopars.ec>
2015-11-17 14:40:02 +0100

    Staging settings

---
+++ project/settings/staging.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from defaults import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'jass-staging.herokuapp.com',
+]
+
+SITE_ID = 2  # Local=1, Staging=2, Prod=3
+
+WS4REDIS_CONNECTION = {
+    'host': 'pub-redis-13828.eu-west-1-2.2.ec2.garantiadata.com',
+    'port': 13828,
+    'db': 0,
+    'password': 'GOFxkqXF1LZM3ZbV',
+}
