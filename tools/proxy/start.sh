3cd8b8fa87866297b3baa67ccf57809009d4fe9d
Cédric Foellmi <cedric@onekilopars.ec>
2015-11-18 13:59:05 +0100

    Revert "Revert "Tweaks""
    This reverts commit a6e788e6d72096c8357c68cdb6dbb7220d3c52a1.

--- tools/proxy/start.sh
+++ tools/proxy/start.sh
@@ -1,7 +1,8 @@
-#! /bin/bash
+#! /bin/sh
 PORT=8080
-JASS_PROD_SERVER="http://jass-prod.herokuapp.com"
-JASS_STAGING_SERVER="http://jass-staging.herokuapp.com"
+JASS_API="/api/inputrequests/"
+JASS_SERVER_1="http://jass-prod.herokuapp.com$JASS_API"
+JASS_SERVER_2="http://jass-staging.herokuapp.com$JASS_API"
 
 if ! which mitmdump >/dev/null ; then
   cat <<EOF
@@ -17,16 +18,11 @@ if ! python test_imports.py ; then
   exit
 fi
 
-JASS_SERVER=$JASS_PROD_SERVER
-if [[ "$1" == "staging" || "$1" == "2" ]] ; then
-  JASS_SERVER=$JASS_STAGING_SERVER
-fi
-
 echo Proxy started. The IP address to use as Manual Proxy in test devices is
 for i in `ifconfig |grep inet |grep -v inet6 |cut -d ' ' -f 2` ; do
   echo $i:8080
 done
-echo You can see the results at: $JASS_SERVER
+echo "You can see the results at: $JASS_SERVER_1."
+echo "                          : $JASS_SERVER_2."
 
-JASS_API="$JASS_SERVER/api/inputrequests/"
-mitmdump -p $PORT -s "sendToServer.py $JASS_API"
+mitmdump -p $PORT -s "sendToServer.py $JASS_SERVER_2" -s "sendToServer.py $JASS_SERVER_1"
