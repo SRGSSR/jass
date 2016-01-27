c3723470af10dcef4d5ea92d2d1f00d6e63d0666
Cédric Foellmi <cedric@onekilopars.ec>
2016-01-27 14:10:15 +0100

    Moving loading spinner directives to utils where it belongs.

--- project/static/javascripts/utils/utils.module.js
+++ project/static/javascripts/utils/utils.module.js
@@ -3,10 +3,15 @@
 
     angular
         .module('jass.utils', [
-            'jass.utils.services'
+            'jass.utils.services',
+            'jass.utils.directives'
         ]);
 
     angular
         .module('jass.utils.services', []);
+
+    angular
+        .module('jass.utils.directives', []);
+
 })();
 
