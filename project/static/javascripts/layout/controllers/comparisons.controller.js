e463033f9b2801ec7537ddc451cd6bc310b78807
Cédric Foellmi <cedric@onekilopars.ec>
2016-01-27 14:08:37 +0100

    No need to use a directive for that

--- project/static/javascripts/layout/controllers/comparisons.controller.js
+++ project/static/javascripts/layout/controllers/comparisons.controller.js
@@ -10,7 +10,7 @@
     function ComparisonsController($scope, $routeParams, InputRequests, RequestIcons, Snackbar) {
         var vm = this;
 
-        vm.comparisontable = undefined;
+        vm.table = undefined;
 
         activate();
 
@@ -24,21 +24,21 @@
 
             $scope.viewLoading = true;
 
-            vm.comparisontable = {};
-            vm.comparisontable.media_id = $routeParams.media_id;
-            vm.comparisontable.event = $routeParams.event;
+            vm.table = {};
+            vm.table.media_id = $routeParams.media_id;
+            vm.table.event = $routeParams.event;
 
-            var url = 'ns_st_ci='+vm.comparisontable.media_id;
-            if (vm.comparisontable.event !== undefined) {
-                url += '&ns_st_ev='+vm.comparisontable.event;
+            var url = 'ns_st_ci='+vm.table.media_id;
+            if (vm.table.event !== undefined) {
+                url += '&ns_st_ev='+vm.table.event;
             }
 
             InputRequests.all(url).then(successFn, errorFn);
 
             function successFn(data, status, headers, config) {
                 $scope.viewLoading = false;
-                vm.comparisontable.requests = [];
-                vm.comparisontable.rows = [];
+                vm.table.requests = [];
+                vm.table.rows = [];
 
                 // Handling pagination of the data.
                 var all_requests = undefined;
@@ -49,74 +49,76 @@
                     all_requests = data.data;
                 }
 
-                // Taking oldest request as reference
-                vm.comparisontable.requests.push(all_requests[all_requests.length-1]);
+                if (all_requests.length > 11) {
+                    // Taking oldest request as reference
+                    vm.table.requests.push(all_requests[all_requests.length-1]);
 
-                // Making comparison of last 10 requests.
-                var limit = Math.min(10, all_requests.length-1);
-                for (var l = 0; l < limit; l++) {
-                    vm.comparisontable.requests.push(all_requests[l]);
-                }
+                    // Making comparison of last 10 requests.
+                    var limit = Math.min(10, all_requests.length-1);
+                    for (var l = 0; l < limit; l++) {
+                        vm.table.requests.push(all_requests[l]);
+                    }
 
-                // First decompose all url arguments and fill a set of keys...
-                // URL arguments are put into a request_arguments object.
-                var keys = [];
-                for (var i = 0; i < vm.comparisontable.requests.length; i++) {
-                    var inputrequest = vm.comparisontable.requests[i];
+                    // First decompose all url arguments and fill a set of keys...
+                    // URL arguments are put into a request_arguments object.
+                    var keys = [];
+                    for (var i = 0; i < vm.table.requests.length; i++) {
+                        var inputrequest = vm.table.requests[i];
 
-                    (function(req, ks) {
-                        var parser = document.createElement('a');
-                        parser.href = req.url;
-                        var tmp_arguments = parser.search.split('&');
-                        req.request_arguments = {};
-                        for (var j = 0; j < tmp_arguments.length; j++) {
-                            var arg = tmp_arguments[j].split('=');
-                            if (arg[0][0] === "?") {
-                                arg[0] = arg[0].substring(1);
+                        (function(req, ks) {
+                            var parser = document.createElement('a');
+                            parser.href = req.url;
+                            var tmp_arguments = parser.search.split('&');
+                            req.request_arguments = {};
+                            for (var j = 0; j < tmp_arguments.length; j++) {
+                                var arg = tmp_arguments[j].split('=');
+                                if (arg[0][0] === "?") {
+                                    arg[0] = arg[0].substring(1);
+                                }
+                                req.request_arguments[arg[0]] = arg[1];
+                                ks.push(arg[0]);
                             }
-                            req.request_arguments[arg[0]] = arg[1];
-                            ks.push(arg[0]);
+                            RequestIcons.findAll(req);
+                        })(inputrequest, keys);
+                    }
+
+                    var filtered_keys = keys.filter(function(elem, index, self) {
+                        return index == self.indexOf(elem);
+                    }).sort();
+
+                    for (var j = 0; j < filtered_keys.length; j++) {
+                        var key = filtered_keys[j];
+                        var row = [];
+                        var key_cell = {'content': key, 'validation': 'unknown'};
+                        row.push(key_cell);
+
+                        var row_items = [];
+                        for (var k = 0; k < vm.table.requests.length; k++) {
+                            var cell = {};
+                            var req = vm.table.requests[k];
+                            if (req.request_arguments[key] != undefined) {
+                                cell['content'] = req.request_arguments[key];
+                            }
+                            else {
+                                cell['content'] = 'missing';
+                            }
+                            row_items.push(cell['content']);
+                            row.push(cell);
                         }
-                        RequestIcons.findAll(req);
-                    })(inputrequest, keys);
-                }
 
-                var filtered_keys = keys.filter(function(elem, index, self) {
-                    return index == self.indexOf(elem);
-                }).sort();
+                        var filtered_row_items = row_items.filter(function(elem, index, self) {
+                            return index == self.indexOf(elem);
+                        });
 
-                for (var j = 0; j < filtered_keys.length; j++) {
-                    var key = filtered_keys[j];
-                    var row = [];
-                    var key_cell = {'content': key, 'validation': 'unknown'};
-                    row.push(key_cell);
-
-                    var row_items = [];
-                    for (var k = 0; k < vm.comparisontable.requests.length; k++) {
-                        var cell = {};
-                        var req = vm.comparisontable.requests[k];
-                        if (req.request_arguments[key] != undefined) {
-                            cell['content'] = req.request_arguments[key];
+                        if (filtered_row_items.length == 1) {
+                            key_cell['validation'] = "ok";
                         }
                         else {
-                            cell['content'] = 'missing';
+                            key_cell['validation'] = "different";
                         }
-                        row_items.push(cell['content']);
-                        row.push(cell);
-                    }
 
-                    var filtered_row_items = row_items.filter(function(elem, index, self) {
-                        return index == self.indexOf(elem);
-                    });
-
-                    if (filtered_row_items.length == 1) {
-                        key_cell['validation'] = "ok";
+                        vm.table.rows.push(row);
                     }
-                    else {
-                        key_cell['validation'] = "different";
-                    }
-
-                    vm.comparisontable.rows.push(row);
                 }
             }
 
