fc7251c9785a30cef0c9839acfdb191f4180d67a
Sebastien Chauvin <sebastien.chauvin@gmail.com>
2016-02-01 12:13:25 +0100

    Persistent database connection (for performance, see: https://devcenter.heroku.com/articles/django-app-configuration#database-connection-persistence ) Log activated

--- project/settings/defaults.py
+++ project/settings/defaults.py
@@ -102,6 +102,8 @@ SESSION_REDIS_PREFIX = 'session'
 import dj_database_url
 DATABASES = { 'default': dj_database_url.config(default=os.environ['DATABASE_URL']) }
 
+# Enable Persistent Connections
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -148,3 +150,36 @@ BOWER_INSTALLED_APPS = (
     'snackbarjs',
     'bootstrap-horizon'
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
